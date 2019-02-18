c40baff30bd37e2dc08c6a4a43c951d963c44bb1
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2019-02-18 18:10:12 +0900

    Bump version

--- Formula/sqlitebiter.rb
+++ Formula/sqlitebiter.rb
@@ -1,9 +1,9 @@
 class Sqlitebiter < Formula
   desc "sqlitebiter is a CLI tool to convert CSV / Excel / HTML / JSON / Jupyter Notebook / LDJSON / LTSV / Markdown / SQLite / SSV / TSV / Google-Sheets to a SQLite database file."
   homepage "https://github.com/thombashi/sqlitebiter"
-  url "https://github.com/thombashi/sqlitebiter/releases/download/v0.23.8/sqlitebiter_macos_amd64.tar.gz"
-  version "0.23.8"
-  sha256 "ca0da8dccef855ebc80e216405ae0af75fc348a8306c0e5c60e334432e4670fe"
+  url "https://github.com/thombashi/sqlitebiter/releases/download/v0.24.0/sqlitebiter_macos_amd64.tar.gz"
+  version "0.24.0"
+  sha256 "33f30697b888543087e4bfd5f9b1fb23c5e55e816e7704c2bba126100b6de243"
 
   def install
     bin.install "sqlitebiter"
